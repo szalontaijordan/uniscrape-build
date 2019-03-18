fdbd41e33292602afb3b5157a524d5d7b48f2be6
Szalontai Jordán <jordanlt1111@gmail.com>
2019-03-19 00:54:48 +0100

    small fix

--- install.js
+++ install.js
@@ -3,8 +3,8 @@
 
     if (!fs.existsSync(`./uniscrape-${repoId}`)) {
         console.log(`[uniscrape] cloning uniscrape-${repoId}`);
-        const repo = clone(`https://github.com/szalontaijordan/uniscrape-${repoId}.git`, `uniscrape-${repoId}`);
-
+        const repo = clone(`https://github.com/szalontaijordan/uniscrape-${repoId}.git`, `./uniscrape-${repoId}`);
+        console.log((await exec('ls')).stdout)
         console.log(`[uniscrape] installing uniscrape-${repoId}`);
         const { stdout, stderr } = await exec(`cd uniscrape-${repoId} && npm install && npm run build`);
         stdout ? console.log(stdout) : console.log(stderr);
