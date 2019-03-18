21afa3a8aa2bf7795550e3babdd7258d5eefa90b
Szalontai Jordán <jordanlt1111@gmail.com>
2019-03-19 00:59:30 +0100

    small fix

--- install.js
+++ install.js
@@ -3,11 +3,14 @@
 
     if (!fs.existsSync(`./uniscrape-${repoId}`)) {
         console.log(`[uniscrape] cloning uniscrape-${repoId}`);
-        const repo = clone(`https://github.com/szalontaijordan/uniscrape-${repoId}.git`, `./uniscrape-${repoId}`);
-        console.log((await exec('ls')).stdout)
-        console.log(`[uniscrape] installing uniscrape-${repoId}`);
-        const { stdout, stderr } = await exec(`cd uniscrape-${repoId} && npm install && npm run build`);
-        stdout ? console.log(stdout) : console.log(stderr);
+        const repo = clone(`https://github.com/szalontaijordan/uniscrape-${repoId}.git`, `./uniscrape-${repoId}`, {}, () => {
+            console.log(`[uniscrape] cloned uniscrape-${repoId}`);
+            console.log((await exec('ls')).stdout)
+            console.log(`[uniscrape] installing uniscrape-${repoId}`);
+            const { stdout, stderr } = await exec(`cd uniscrape-${repoId} && npm install && npm run build`);
+
+            stdout ? console.log(stdout) : (() => { throw stderr })();
+        });
     } else {
         console.log(`[uniscrape] uniscrape-${repoId} already exists`);
     }
